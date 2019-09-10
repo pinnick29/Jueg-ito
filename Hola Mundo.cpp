462b6741adbf60de5b909727e0cb1a7edcb5eab1
Sergio lopez <al376536@uji.es>
2019-09-10 16:33:07 +0200

    added name

---
+++ Hola Mundo.cpp
@@ -0,0 +1,16 @@
+
+using namespace std;
+
+// main function - 
+// where the execution of program begins 
+int main()
+{
+	string nombre; 
+
+	cout << "Hola, introduzca su nombre";
+
+	cin >> nombre;
+
+	cout << "Hola," << nombre << ".";
+
+}
